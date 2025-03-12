8df9e920645da63e2902dcc8e594a56413f19387
Ethan Cheung <echeungy2001@gmail.com>
2025-03-12 14:13:14 +0000

    Update Incentive.py

--- Incentive.py
+++ Incentive.py
@@ -2,14 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def myinc(file1, file2):
+        # This block of code reads the excel files for the data and the point system
         data=pd.read_excel(file1)
         ind=pd.read_excel(file2)
         ipoint = [x for x in ind['1 point'].astype(str).tolist() if pd.notna(x) and x != 'nan']
         iipoint = [x for x in ind['2 points'].astype(str).tolist() if pd.notna(x) and x != 'nan']
         iiipoint = [x for x in ind['3 points'].astype(str).tolist() if pd.notna(x) and x != 'nan']
+        # A summative row in the sales data is created for the results
         data.loc[len(data)] = [0]* len(data.columns)
+        # Bar team members and Floor team mambers are defined by their index in the table
         bar = ['Oscar J', 'Drew 2', 'Caitlin', 'Cameron', 'Rhian', 'Dan R', 'Scarlet', 'Jakey M', 'Blessing']
         floor = ['Andreea', 'Georgia L', 'Simona', 'Damien', 'Sophie H', 'Ben', 'Halima']
+        # This loop converts the sales data into points for each team member above
         for x in range(0, len(data)-1):
             if data['Description'].values[x] in ipoint:
                 data.iloc[len(data)-1, 2:len(data.columns)-1] = data.iloc[len(data)-1, 2:len(data.columns)-1] + data.iloc[x, 2:len(data.columns)-1]
@@ -23,13 +27,14 @@ def myinc(file1, file2):
                 result[index_name] += points
             else:
                 print(f"Index {index_name} not found in the Series.")
-
+        # Additional points awarded for number of reveiws is manually added using an input
         while True:
             index_name = input("Enter the index to add points to (or 'stop' to end): ")
             if index_name.lower() == 'stop':
                 break
             points = int(input("Enter the number of points to add: "))
             add_points(result, index_name, points)
+        # Using matplotlib, a 2 bar codes are made: one for the bar results and one for the floor results
         bresult=result.index.isin(bar)
         barseries=result[bresult].sort_values(ascending=False)
         fresult=result.index.isin(floor)
